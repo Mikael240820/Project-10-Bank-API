cd96db4acf62748b70d771d80e065ac09cd49deb
Mikael <ocmikaelf@outlook.fr>
2025-12-08 16:10:08 +0100

    Redirect logged user if they are on login page

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,6 +1,19 @@
+import { useEffect } from 'react';
 import SignInForm from '../components/SignInForm';
+import { useNavigate } from 'react-router-dom';
+import useAuth from '../hooks/useAuth';
 
 function SignIn() {
+  const navigate = useNavigate();
+  const { isAuthenticated } = useAuth()
+
+  // Redirection si déjà connecté
+  useEffect(() => {
+    if (isAuthenticated) {
+      navigate('/profile');
+    }
+  }, [isAuthenticated]);
+
   return (
     <main className="main bg-dark">
       <section className="sign-in-content">
